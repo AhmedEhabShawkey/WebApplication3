448b672630632c56371e471d96804b3c552446eb
NTG <NTG@DESKTOP-AOU2TMG>
2024-09-21 21:34:34 +0300

    Add project files.

---
+++ WebApplication2/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication2.Controllers
+
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult GetMovie(int Id,string name) //(int[] arr) //?arr[0]=10 &arr[1]=90
+        {
+            return Content($"Movie with id :{Id} And Name IS : {name}");
+        }
+        public IActionResult index()
+        {
+            return Content ("Hello from index");
+        }
+        public IActionResult test()
+        {
+            return RedirectToAction(nameof(index));
+        }
+
+    }
+}
